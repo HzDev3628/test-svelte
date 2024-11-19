662ea37beae36127b9146739a316c2c56f87ec0e
Artem Kharchuk <artem.khar5uk@gmail.com>
2024-11-19 16:24:55 +0200

    feat: routing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1 class="text-5xl">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<svelte:head>
+  <title>Home page</title>
+</svelte:head>
+
+<main>
+  <h1 class="text-5xl">Welcome to SvelteKit</h1>
+  <a href="/second">Second page</a>
+</main>

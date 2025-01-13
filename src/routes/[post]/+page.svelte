b5ee3a47c346503417b326e4e4a45714aed0f7bb
Artem Kharchuk <artem.khar5uk@gmail.com>
2025-01-13 15:52:17 +0200

    add: post card on the post page

--- src/routes/[post]/+page.svelte
+++ src/routes/[post]/+page.svelte
@@ -3,7 +3,31 @@ import Link from '../components/link.svelte'
 import { page } from '$app/stores'
 
 $: postId = $page.params.post
+
+async function getData() {
+  return await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${$page.params.post}`,
+  )
+    .then((res) => res.json())
+    .catch((err) => console.error(err))
+}
+
+type Post = {
+  id: number
+  title: string
+  body: string
+}
+
+const data = getData() as Promise<Post>
 </script>
 
 <h1>There is an ID on this post: {postId}</h1>
 <Link href="/" title="Home page" />
+<div class="w-full max-w-[340px] bg-[#09090940] p-4 text-start border border-[#F9F9F940] rounded-md font-normal">
+  {#await data}
+    <p>Loading...</p>
+  {:then post} 
+    <h3 class="text-lg font-medium">{post.title}</h3>
+    <span>{post.body}</span>
+  {/await}
+</div>
